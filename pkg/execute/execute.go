4b35ffe314285d1daea92a77d943f3ec17af2f86
Vincent-Omondi <vincentomondi251@gmail.com>
2024-07-15 11:37:58 +0300

    Added pkg/ directory

---
+++ pkg/execute/execute.go
@@ -0,0 +1,65 @@
+package checker
+
+import "errors"
+
+func ExecuteInstruction(instruction string, stackA, stackB *[]int) error {
+	switch instruction {
+	case "sa":
+		swap(stackA)
+	case "sb":
+		swap(stackB)
+	case "ss":
+		swap(stackA)
+		swap(stackB)
+	case "pa":
+		push(stackB, stackA)
+	case "pb":
+		push(stackA, stackB)
+	case "ra":
+		rotate(stackA)
+	case "rb":
+		rotate(stackB)
+	case "rr":
+		rotate(stackA)
+		rotate(stackB)
+	case "rra":
+		reverseRotate(stackA)
+	case "rrb":
+		reverseRotate(stackB)
+	case "rrr":
+		reverseRotate(stackA)
+		reverseRotate(stackB)
+	default:
+		return errors.New("Invalid instruction")
+	}
+	return nil
+}
+
+func swap(stack *[]int) {
+	if len(*stack) < 2 {
+		return
+	}
+	(*stack)[0], (*stack)[1] = (*stack)[1], (*stack)[0]
+}
+
+func push(fromStack, toStack *[]int) {
+	if len(*fromStack) == 0 {
+		return
+	}
+	*toStack = append([]int{(*fromStack)[0]}, *toStack...)
+	*fromStack = (*fromStack)[1:]
+}
+
+func rotate(stack *[]int) {
+	if len(*stack) < 2 {
+		return
+	}
+	*stack = append((*stack)[1:], (*stack)[0])
+}
+
+func reverseRotate(stack *[]int) {
+	if len(*stack) < 2 {
+		return
+	}
+	*stack = append([]int{(*stack)[len(*stack)-1]}, (*stack)[:len(*stack)-1]...)
+}
---
+++ pkg/validate/validate.go
@@ -0,0 +1,33 @@
+package validate
+
+import (
+	"errors"
+	"strconv"
+)
+
+func ParseInput(args []string) ([]int, error) {
+	stack := []int{}
+	seen := map[int]bool{}
+
+	for _, arg := range args {
+		num, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		if seen[num] {
+			return nil, errors.New("duplicate number found")
+		}
+		seen[num] = true
+		stack = append(stack, num)
+	}
+	return stack, nil
+}
+
+func IsSorted(stack []int) bool {
+	for i := 0; i < len(stack)-1; i++ {
+		if stack[i] > stack[i+1] {
+			return false
+		}
+	}
+	return true
+}
