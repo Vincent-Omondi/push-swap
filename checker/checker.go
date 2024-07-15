e59355f15aafd177a84823c4bf9e5f3707cef94d
Vincent-Omondi <vincentomondi251@gmail.com>
2024-07-15 15:28:41 +0300

    fix: resolve issue allowing unauthorized input reading

--- checker/checker.go
+++ checker/checker.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/push-swap/pkg/execute"
 	"github.com/push-swap/pkg/validate"
@@ -14,9 +15,12 @@ func main() {
 		return
 	}
 
-	stackA, err := validate.ParseInput(os.Args[1:])
+	// Split the input args into individual numbers
+	inputArgs := strings.Split(os.Args[1], " ")
+	stackA, err := validate.ParseInput(inputArgs)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error")
+		return
 	}
 
 	stackB := []int{}
@@ -31,11 +35,18 @@ func main() {
 		instructions = append(instructions, instruction)
 	}
 
+	// Debug print before executing instructions
+	fmt.Println("Initial stackA:", stackA)
+	fmt.Println("Initial stackB:", stackB)
+	fmt.Println("Instructions:", instructions)
+
 	for _, instruction := range instructions {
 		if err := execute.ExecuteInstruction(instruction, &stackA, &stackB); err != nil {
 			fmt.Fprintln(os.Stderr, "Error")
 			return
 		}
+		// Debug print after each instruction
+		fmt.Println("After", instruction, "stackA:", stackA, "stackB:", stackB)
 	}
 
 	if validate.IsSorted(stackA) && len(stackB) == 0 {
