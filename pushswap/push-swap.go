43c2c7dbfc35ca9869baf6f4b2c8e1e6fb59af3c
Vincent-Omondi <vincentomondi251@gmail.com>
2024-07-16 15:40:10 +0300

    fix: reduced the number of instructions

--- pushswap/push-swap.go
+++ pushswap/push-swap.go
@@ -35,19 +35,30 @@ func addInstruction(instruction string, stackA, stackB *[]int, instructions *[]s
 	execute.ExecuteInstruction(instruction, stackA, stackB)
 }
 
+// Function to find the index of the smallest element
+func findMinIndex(stack []int) int {
+	minIndex := 0
+	for i := 1; i < len(stack); i++ {
+		if stack[i] < stack[minIndex] {
+			minIndex = i
+		}
+	}
+	return minIndex
+}
+
 // Function to generate the sequence of instructions
 func generateInstructions(stackA []int) []string {
 	stackB := []int{}
 	instructions := []string{}
 
-	// Push all elements to stack B, keeping it sorted
+	// If the stack is already sorted, return immediately
+	if validate.IsSorted(stackA) {
+		return instructions
+	}
+
 	for len(stackA) > 0 {
-		minIndex := 0
-		for i := 1; i < len(stackA); i++ {
-			if stackA[i] < stackA[minIndex] {
-				minIndex = i
-			}
-		}
+		minIndex := findMinIndex(stackA)
+
 		// Bring the smallest element to the top
 		if minIndex > len(stackA)/2 {
 			for i := len(stackA) - 1; i >= minIndex; i-- {
@@ -58,8 +69,14 @@ func generateInstructions(stackA []int) []string {
 				addInstruction("ra", &stackA, &stackB, &instructions)
 			}
 		}
+
 		// Push the smallest element to stack B
 		addInstruction("pb", &stackA, &stackB, &instructions)
+
+		// Try to sort within stack A
+		if len(stackA) > 1 && stackA[0] > stackA[1] {
+			addInstruction("sa", &stackA, &stackB, &instructions)
+		}
 	}
 
 	// Push all elements back to stack A
